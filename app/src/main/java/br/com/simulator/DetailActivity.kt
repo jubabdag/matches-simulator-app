7eebb0548bf0bde12c350122579fe7734f5539e3
jubabdag <yoshikawa.juvenal@gmail.com>
2022-03-03 20:25:42 -0300

    Desenvolvimento dos layouts de listagem e detalhes de partidas

---
+++ app/src/main/java/br/com/simulator/DetailActivity.kt
@@ -0,0 +1,19 @@
+package br.com.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
